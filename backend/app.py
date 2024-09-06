fc93df3865d096da69606f0892e34077ef4a2240
SamirKhan21860 <msamirkhan561@gmail.com>
2024-09-06 17:21:33 +0500

    default route created using flask

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,18 @@
-print("app is running.")
+from flask import Flask, request, render_template
+
+# Create a Flask web application instance
+app = Flask(__name__)
+
+# Configure session settings
+
+# Connect to the SQLite database here
+
+@app.route("/", methods=['GET'])
+def index():
+    
+    render_template("index.html")
+
+
+# Run the Flask application in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
