78b01dfe3de898d554dd69a2b2c9c9d7c47383b7
SamirKhan21860 <msamirkhan561@gmail.com>
2024-09-25 12:58:19 +0500

    Workin on signup backend logic

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
 # Import necessary modules
 import os
 import requests
-from flask import jsonify
+# from flask import jsonify
 from flask import Flask, request, redirect, url_for, render_template, session
-from flask_session import Session
+# from flask_session import Session
 from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import flash_message, login_required, generate_secret_key, validate_class_name, check_required_fields, get_data_from_db, get_class_timing, get_exam_schedules
@@ -12,27 +12,27 @@ from helpers import flash_message, login_required, generate_secret_key, validate
 app = Flask(__name__)
 
 # Configure session settings
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-app.secret_key = generate_secret_key(64)
-Session(app)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# app.secret_key = generate_secret_key(64)
+# Session(app)
 
 # Connect to the SQLite database here
 db = SQL("sqlite:///footo.db")
 
 @app.route("/", methods=['GET', 'POST'])
-@login_required
+# @login_required
 def index():
     # Retrieve assignments for the logged-in student
-    assignment = db.execute("SELECT * FROM assignment WHERE student_id = ?", session["user_id"])
+    # assignment = db.execute("SELECT * FROM assignment WHERE student_id = ?", session["user_id"])
     # Render the homepage template with assignment data
-    return render_template("index.html", assignment=assignment)
+    return {"Default:": "home route"}
 
 # Define route for user signup
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     # Clear any existing session data
-    session.clear()
+    # session.clear()
 
     # Handle POST request for user signup
     if request.method == "POST":
@@ -40,36 +40,37 @@ def signup():
         full_name = request.form.get("full_name")
         email = request.form.get("email")
         password = request.form.get("password")
-        class_name = request.form.get("class")
+        profession = request.form.get("profession")
 
         # Check if all required fields are provided
-        result = check_required_fields({"Full Name": full_name, "Email": email, "Password": password, "Class Name": class_name}, current_page="signup")
+        result = check_required_fields({"Full Name": full_name, "Email": email, "Password": password, "profession": profession}, current_page="signup")
         if result:
             return result
 
         # Validate the format of the class name
-        if not validate_class_name(class_name):
-            flash_message("Class Name must adhere to the specified convention. i.e(CS50, CS or any acronym)", category='info')
-            return render_template("signup.html")
+        # if not validate_class_name(class_name):
+        #     flash_message("Class Name must adhere to the specified convention. i.e(CS50, CS or any acronym)", category='info')
+        #     return render_template("signup.html")
 
         # Check if the provided email already exists in the database
-        email_exists = db.execute("SELECT * FROM students WHERE email = ?", email)
+        email_exists = db.execute("SELECT * FROM users WHERE email = ?", email)
         if email_exists:
             flash_message("Email is already exist. Please Login Instead.", category='info')
-            return render_template("signup.html")
+            redirect("/signup")
+            return {"error": "Eamil is already exist. Please Login Instead."}
 
         # Hash the password before storing it in the database
         hash_password = generate_password_hash(password)
 
         # Insert user data into the database
-        db.execute("INSERT INTO students (full_name, email, password, class_name, role) VALUES (?, ?, ?, ?, ?)", full_name, email, hash_password, class_name, 'student')
+        db.execute("INSERT INTO users (name, email, password, profession) VALUES (?, ?, ?, ?, ?)", full_name, email, hash_password, profession)
 
         # Display a success message and redirect to the login page
         flash_message("Signup successful!", category='success')
-        return render_template("login.html")
+        return redirect("/login")
 
     # Render the signup page for GET requests
-    return render_template("signup.html")
+    return url_for("/signup")
 
 # Define route for user login
 @app.route("/login", methods=["GET", "POST"])
--- backend/data.sql
+++ backend/data.sql
@@ -2,25 +2,27 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    name TEXT NOT NULL,
+    profession TEXT NOT NULL,
 );
 
-CREATE TABLE projects (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT,
-    -- images
-    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
+-- CREATE TABLE projects (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     title TEXT NOT NULL,
+--     description TEXT,
+--     images BLOB NOT NULL,
+--     uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     user_id INTEGER NOT NULL,
+--     FOREIGN KEY (user_id) REFERENCES users (id)
+-- );
 
-CREATE TABLE socials (
-    user_id INTEGER NOT NULL,
-    facebook TEXT,
-    twitter TEXT,
-    linedin TEXT,
-    pintrest TEXT,
-    instagram TEXT,
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
+-- CREATE TABLE socials (
+--     user_id INTEGER NOT NULL,
+--     facebook TEXT,
+--     twitter TEXT,
+--     linedin TEXT,
+--     pintrest TEXT,
+--     instagram TEXT,
+--     FOREIGN KEY (user_id) REFERENCES users (id)
+-- );
